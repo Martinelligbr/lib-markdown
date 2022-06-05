1e3166e15546559207b499dcbf4d42a569e0686e
martinelligbr <gabrielmartinellids@gmail.com>
2022-06-04 22:44:05 -0300

    adicionado test na aplicação

--- LIB-MARKDOWN/index.js
+++ LIB-MARKDOWN/index.js
@@ -1,36 +1,28 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+function extraiLinks(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+  while((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] })
+  }
+  return arrayResultados.length === 0 ? 'Não há links' : arrayResultados;
+}
+
 function trataErro(erro) {
-  throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
+  throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho'))
 }
 
 async function pegaArquivo(caminhoDoArquivo) {
-  const encoding = 'utf-8';
+  console.log(caminhoDoArquivo)
   try {
-    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-    console.log(chalk.green(texto))
+    const texto = await fs.promises.readFile(caminhoDoArquivo, 'utf-8')
+    return extraiLinks(texto);
   } catch(erro) {
     trataErro(erro);
   }
 }
 
-// function pegaArquivo(caminhoDoArquivo) {
-//   const encoding = 'utf-8';
-//   fs.promises
-//   .readFile(caminhoDoArquivo, encoding)
-//   .then((texto) => chalk.green(console.log(texto)))
-//   .catch((erro) => trataErro(erro))
-// }
-
-// function pegaArquivo(caminhoDoArquivo) {
-//   const encoding = 'utf-8';
-//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-//     if (erro) {
-//       trataErro(erro);
-//     }
-//     console.log(chalk.green(texto));
-//   })
-// }
-
-pegaArquivo('./arquivos/texto1.md');
+module.exports = pegaArquivo
